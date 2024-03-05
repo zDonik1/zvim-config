8bbc9bd90c6266e41ca91b8410684e4d8ad8b254
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2024-03-05 06:21:11 +0500

    refactor: closely placed LSP related plugin

--- lua/zdonik/lazy_plugins.lua
+++ lua/zdonik/lazy_plugins.lua
@@ -239,49 +239,6 @@ local plugins = {
 	},
 
 	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		init = function()
-			require("lsp-zero").on_attach(function(_, bufnr)
-				local opts = { buffer = bufnr, remap = false }
-
-				vim.keymap.set("n", "gd", function()
-					vim.lsp.buf.definition()
-				end, opts)
-				vim.keymap.set("n", "K", function()
-					vim.lsp.buf.hover()
-				end, opts)
-				vim.keymap.set("n", "<leader>vks", function()
-					vim.lsp.buf.workspace_symbol()
-				end, opts)
-				vim.keymap.set("n", "<leader>vd", function()
-					vim.diagnostic.open_float()
-				end, opts)
-				vim.keymap.set("n", "]d", function()
-					vim.diagnostic.goto_next()
-				end, opts)
-				vim.keymap.set("n", "[d", function()
-					vim.diagnostic.goto_prev()
-				end, opts)
-				vim.keymap.set({ "n", "v" }, "<leader>vca", function()
-					vim.lsp.buf.code_action()
-				end, opts)
-				vim.keymap.set("n", "<leader>vrr", function()
-					vim.lsp.buf.references()
-				end, opts)
-				vim.keymap.set("n", "<leader>vrn", function()
-					vim.lsp.buf.rename()
-				end, opts)
-				vim.keymap.set("i", "<C-h>", function()
-					vim.lsp.buf.signature_help()
-				end, opts)
-			end)
-		end,
-	},
-
-	{ "neovim/nvim-lspconfig" },
-
-	{
 		"hrsh7th/nvim-cmp",
 		event = { "InsertEnter", "CmdlineEnter" },
 		dependencies = {
@@ -367,6 +324,47 @@ local plugins = {
 	{ "L3MON4D3/LuaSnip" },
 
 	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		init = function()
+			require("lsp-zero").on_attach(function(_, bufnr)
+				local opts = { buffer = bufnr, remap = false }
+
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, opts)
+				vim.keymap.set("n", "K", function()
+					vim.lsp.buf.hover()
+				end, opts)
+				vim.keymap.set("n", "<leader>vks", function()
+					vim.lsp.buf.workspace_symbol()
+				end, opts)
+				vim.keymap.set("n", "<leader>vd", function()
+					vim.diagnostic.open_float()
+				end, opts)
+				vim.keymap.set("n", "]d", function()
+					vim.diagnostic.goto_next()
+				end, opts)
+				vim.keymap.set("n", "[d", function()
+					vim.diagnostic.goto_prev()
+				end, opts)
+				vim.keymap.set({ "n", "v" }, "<leader>vca", function()
+					vim.lsp.buf.code_action()
+				end, opts)
+				vim.keymap.set("n", "<leader>vrr", function()
+					vim.lsp.buf.references()
+				end, opts)
+				vim.keymap.set("n", "<leader>vrn", function()
+					vim.lsp.buf.rename()
+				end, opts)
+				vim.keymap.set("i", "<C-h>", function()
+					vim.lsp.buf.signature_help()
+				end, opts)
+			end)
+		end,
+	},
+
+	{
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup({})
@@ -375,6 +373,7 @@ local plugins = {
 
 	{
 		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "neovim/nvim-lspconfig" },
 		config = function()
 			local lsp_zero = require("lsp-zero")
 			lsp_zero.extend_lspconfig()
